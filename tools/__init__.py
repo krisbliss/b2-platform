2e01380f63cbc7f7dbba5bed6780ebbbc8de3490
AnkitGole <aggole@wpi.edu>
2026-06-19 16:01:15 -0400

    chore(poc-1): add death_certificate_pipeline.yaml tool manifest

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,4 +1,6 @@
 """Tools package."""
 
-from .death_certificate_consistency import analyze_death_certificate_consistency
-
+from .death_certificate_pipeline.death_certificate_consistency import (
+    analyze_death_certificate_consistency,
+    analyze_death_certificate_consistency_base64,
+)
